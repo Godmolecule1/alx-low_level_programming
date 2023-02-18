bc100e2b86a19877a348c39a13c1a0473e78a228
god_molecule <ogierhiakhievans@rocketmail.com>
2023-02-18 06:01:10 +0100

    variables

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -13,15 +13,15 @@
 
 int main(void)
 {
-	char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
-	int len = strlen(alphabets);
-	int i;
+char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
+int len = strlen(alphabets);
+int i;
 
-	for (i = 0; i < len; i++)
-	{
-	putchar(alphabets[i]);	
-	}
-	putchar('\n');
+for (i = 0; i < len; i++)
+{
+putchar(alphabets[i]);
+}
+putchar('\n');
 
-	return (0);
+return (0);
 }
