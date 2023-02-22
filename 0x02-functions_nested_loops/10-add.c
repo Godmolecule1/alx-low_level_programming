68c2e0223b43c6b39eb9c0e81c299997cb18b7fa
god_molecule <ogierhiakhievans@rocketmail.com>
2023-02-23 00:29:37 +0100

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - funtion that adds two integer
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+
+int add(int a, int b)
+
+{
+
+	int result;
+
+	result = a + b;
+	return (result);
+}
