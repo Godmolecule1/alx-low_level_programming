2014c9c6d687b742eae4f4f08281a2397de807b5
god_molecule <ogierhiakhievans@rocketmail.com>
2023-02-23 16:58:20 +0100

    print numbers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,11 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * mul -  function that multiplies two integers
  * @a: number
  * @b: number
- * Return: result
+ * Return: a multiplied by b 
  */
-
 int mul(int a, int b)
 {
 	return(a * b);
